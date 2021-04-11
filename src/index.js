e6178092555b0416d084feba3c1c7732c4909a4f
BuiTiep379 <buitiep379@gmail.com>
2021-04-11 21:24:04 +0700

    add

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-var exphbs = require('express-handlebars');
-const app = express();
-const route = require('./routes');
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const app = express()
+const route = require('./routes')
 
-const port = 3000;
+const port = 3000
 // parse application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }))
 // parse application/json
-app.use(express.json());
+app.use(express.json())
 
-app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname, 'public')))
 // themplate engine
 app.engine(
-    'hbs',
-    exphbs({
-        extname: '.hbs',
-    }),
-);
-app.set('view engine', 'hbs');
-
-app.set('views', path.join(__dirname, 'resources/views'));
+  'hbs',
+  exphbs({
+    extname: '.hbs'
+  })
+)
+debugger
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources/views'))
 // route init
-    route(app);
-    app.listen(port, () => {
-        console.log(`Example app listening at http://localhost:${port}`);
-    });
+route(app)
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
