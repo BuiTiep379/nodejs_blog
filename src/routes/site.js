25c29bd7bb8bee38a798421ad79d96da1c45b34c
BuiTiep379 <buitiep379@gmail.com>
2021-04-11 17:34:48 +0700

    mmm

--- src/routes/site.js
+++ src/routes/site.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const siteConstroller = require('../app/controllers/SiteController');
+            const siteConstroller = require('../app/controllers/SiteController');
 
 router.use('/search', siteConstroller.search);
 router.use('/', siteConstroller.index);
