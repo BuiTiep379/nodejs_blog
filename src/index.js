945288391604b719566b85519ba9cd83787be6fa
BuiTiep379 <buitiep379@gmail.com>
2021-04-11 17:56:39 +0700

    tiep4

--- src/index.js
+++ src/index.js
@@ -20,11 +20,11 @@ app.engine(
         extname: '.hbs',
     }),
 );
-        app.set('view engine', 'hbs');
+app.set('view engine', 'hbs');
 
-        app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources/views'));
 // route init
-route(app);
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+        route(app);
+        app.listen(port, () => {
+            console.log(`Example app listening at http://localhost:${port}`);
+        });
